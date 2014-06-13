10fe9e8d28b4013d1ea60e53d6405aca81defcb7
Ricardo Monti <ricardo.monti08@gmail.com>
2014-06-13 18:37:41 +0100

    fixed cython setup

--- pySINGLE/setup.py
+++ pySINGLE/setup.py
@@ -5,5 +5,6 @@ import numpy
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("Z_shooting", ["Z_shooting.pyx"], include_dirs=[numpy.get_include()])]
+    ext_modules = [Extension("Z_shooting", ["Z_shooting.pyx"],)],
+    include_dirs=[numpy.get_include(),],
 )
